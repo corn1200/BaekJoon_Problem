55539d43e3f465e58458472cf05ab6d41848bde5
corn1200 <corn120033@gmail.com>
2021-02-04 18:48:03 +0900

    feat: 나머지 문제 풀이를 완료한다

--- src/ProblemOneDimenArray/Remainder.java
+++ src/ProblemOneDimenArray/Remainder.java
@@ -1,7 +1,22 @@
 package ProblemOneDimenArray;
 
-public class Remainder {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.HashSet;
 
+public class Remainder {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        HashSet<Integer> remainder = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            remainder.add(Integer.parseInt(br.readLine()) % 42);
+        }
+        br.close();
+
+        bw.write(Integer.toString(remainder.size()));
+
+        bw.flush();
+        bw.close();
     }
 }
