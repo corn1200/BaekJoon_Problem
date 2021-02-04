4d67343f33d2e33af8bfed4e68f46f86271af53a
corn1200 <corn120033@gmail.com>
2021-02-04 21:08:09 +0900

    feat: 평균 문제 풀이를 완료한다

--- src/ProblemOneDimenArray/Average.java
+++ src/ProblemOneDimenArray/Average.java
@@ -1,7 +1,35 @@
 package ProblemOneDimenArray;
 
-public class Average {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Average {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        double highGrade = 0;
+        double newSumGrade = 0;
+        double newAvgGrade;
+        int subject = Integer.parseInt(br.readLine());
+        double[] grade = new double[subject];
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        for (int i = 0; stk.hasMoreTokens(); i++) {
+            grade[i] = Integer.parseInt(stk.nextToken());
+            highGrade = grade[i] > highGrade ? grade[i] : highGrade;
+        }
+
+        for (double subjectGrade : grade) {
+            newSumGrade += subjectGrade / highGrade * 100;
+        }
+
+        newAvgGrade = newSumGrade / subject;
+
+        bw.write(String.valueOf(newAvgGrade));
+
+        bw.flush();
+        bw.close();
     }
 }
