530ff64c0dce768e1b28250027cf4fe12f0586da
corn1200 <corn120033@gmail.com>
2021-02-05 20:21:03 +0900

    feat: OX 퀴즈 문제 풀이를 완료한다

--- src/ProblemOneDimenArray/OxQuiz.java
+++ src/ProblemOneDimenArray/OxQuiz.java
@@ -1,7 +1,37 @@
 package ProblemOneDimenArray;
 
-public class OxQuiz {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class OxQuiz {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arrayCount = Integer.parseInt(br.readLine());
+        String[] testCaseArray = new String[arrayCount];
+
+        for (int i = 0; i < testCaseArray.length; i++) {
+            testCaseArray[i] = br.readLine();
+        }
+        br.close();
+
+        for (String testCase : testCaseArray) {
+            int totalScore = 0;
+            int streakScore = 1;
+            char[] charAtCase = testCase.toCharArray();
+            for (char charOfCase : charAtCase) {
+                switch (charOfCase) {
+                    case 'O':
+                        totalScore += streakScore++;
+                        break;
+                    case 'X':
+                        streakScore = 1;
+                        break;
+                }
+            }
+            bw.write(totalScore + "\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
