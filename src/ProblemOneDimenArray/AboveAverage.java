00e58ead9041e9a55d78fc164854c123e9ed3773
corn1200 <corn120033@gmail.com>
2021-02-05 21:18:01 +0900

    feat: 평균은 넘겠지 문제 풀이를 완료한다

--- src/ProblemOneDimenArray/AboveAverage.java
+++ src/ProblemOneDimenArray/AboveAverage.java
@@ -1,7 +1,41 @@
 package ProblemOneDimenArray;
 
-public class AboveAverage {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class AboveAverage {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int C = Integer.parseInt(br.readLine());
+        double[] avgRatio = new double[C];
+
+        for (int i = 0; i < C; i++) {
+            double avgScore;
+            double sumScore = 0;
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(stk.nextToken());
+            double[] stuScoreArray = new double[N];
+            for (int j = 0; j < stuScoreArray.length; j++) {
+                stuScoreArray[j] = Double.parseDouble(stk.nextToken());
+                sumScore += stuScoreArray[j];
+            }
+            avgScore = sumScore / N;
+            double overAvgStu = 0;
+            for (double stuScore : stuScoreArray) {
+                if (stuScore > avgScore) {
+                    overAvgStu++;
+                }
+            }
+            avgRatio[i] = overAvgStu / (double) N * 100;
+        }
+
+        for (double overAvgPercent : avgRatio) {
+            bw.write(String.format("%.3f", overAvgPercent) + "%\n");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
