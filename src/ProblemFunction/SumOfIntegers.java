9ddbb6cb3aec4e771f353c3b91d9662d8c9d06b8
corn1200 <corn120033@gmail.com>
2021-02-05 22:08:18 +0900

    feat: 정수 N개의 합 문제 풀이를 완료한다

--- src/ProblemFunction/SumOfIntegers.java
+++ src/ProblemFunction/SumOfIntegers.java
@@ -4,4 +4,12 @@ public class SumOfIntegers {
     public static void main(String[] args) {
 
     }
+
+    public static long sum(int[] a) {
+        long total = 0;
+        for (int n : a) {
+            total += n;
+        }
+        return total;
+    }
 }
