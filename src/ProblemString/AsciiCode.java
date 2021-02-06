c695cb7140932c1a31fe6570a9d7c9be30df61fd
corn1200 <corn120033@gmail.com>
2021-02-06 23:49:53 +0900

    feat: 아스키 코드 문제 풀이를 완료한다

--- src/ProblemString/AsciiCode.java
+++ src/ProblemString/AsciiCode.java
@@ -1,7 +1,18 @@
 package ProblemString;
 
-public class AsciiCode {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class AsciiCode {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char string = br.readLine().toCharArray()[0];
+        br.close();
+        int asciiCode = string;
+
+        bw.write(String.valueOf(asciiCode));
+        bw.flush();
+        bw.close();
     }
 }
