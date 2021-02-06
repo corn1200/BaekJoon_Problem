7ffe4987254790ebb915d3a20faeff5f60b9ef87
corn1200 <corn120033@gmail.com>
2021-02-07 00:06:46 +0900

    feat: 숫자의 합 문제 풀이를 완료한다

--- src/ProblemString/SumOfNumbers.java
+++ src/ProblemString/SumOfNumbers.java
@@ -1,7 +1,28 @@
 package ProblemString;
 
-public class SumOfNumbers {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class SumOfNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        String numString = br.readLine();
+        char[] numCharArr = numString.toCharArray();
+        br.close();
+
+        bw.write(String.valueOf(charArrToInt(numCharArr)));
+        bw.flush();
+        bw.close();
+    }
+
+    public static int charArrToInt(char[] charArr) {
+        int numSum = 0;
+        for (char num : charArr) {
+            numSum += Character.getNumericValue(num);
+        }
+
+        return numSum;
     }
 }
