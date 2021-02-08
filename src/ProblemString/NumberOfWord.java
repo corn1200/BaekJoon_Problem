07f3701d59e72ff1437cd4029e0e8289c64d8bc4
corn1200 <corn120033@gmail.com>
2021-02-09 01:26:32 +0900

    feat: 단어의 개수 문제 풀이를 완료한다

--- src/ProblemString/NumberOfWord.java
+++ src/ProblemString/NumberOfWord.java
@@ -1,7 +1,18 @@
 package ProblemString;
 
-public class NumberOfWord {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class NumberOfWord {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer newString = new StringTokenizer(br.readLine());
+        br.close();
+
+        bw.write(String.valueOf(newString.countTokens()));
+        bw.flush();
+        bw.close();
     }
 }
