f82f3daa531d0cf6dccf1fbd410866dbe90dc0c7
corn1200 <corn120033@gmail.com>
2021-02-09 01:08:44 +0900

    feat: 단어 공부 문제 풀이를 완료한다

--- src/ProblemString/WordStudy.java
+++ src/ProblemString/WordStudy.java
@@ -1,7 +1,41 @@
 package ProblemString;
 
-public class WordStudy {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.HashMap;
+import java.util.Optional;
 
+public class WordStudy {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine().toLowerCase();
+        br.close();
+        char[] wordCharArr = word.toCharArray();
+        HashMap<Character, Integer> alphaCount = new HashMap<>();
+        int mostUseCount = 0;
+        char mostUseChar = 0;
+        int secondUseCount = 0;
+
+        for (char wordChar : wordCharArr) {
+            Optional<Integer> opt = Optional.ofNullable
+                    (alphaCount.get(wordChar));
+            int wordCount = opt.isPresent() ? opt.get() + 1 : 1;
+            alphaCount.put(wordChar, wordCount);
+            if (mostUseCount < wordCount) {
+                mostUseCount = wordCount;
+                mostUseChar = wordChar;
+            } else if (secondUseCount < wordCount) {
+                secondUseCount = wordCount;
+            }
+        }
+
+        if (mostUseCount == secondUseCount) {
+            bw.write("?");
+        } else {
+            bw.write(String.valueOf(mostUseChar).toUpperCase());
+        }
+        bw.flush();
+        bw.close();
     }
 }
