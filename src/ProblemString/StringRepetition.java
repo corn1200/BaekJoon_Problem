b7f5202bed7fa1289218b3dff7bacbe9d5e1b167
corn1200 <corn120033@gmail.com>
2021-02-08 23:08:54 +0900

    feat: 문자열 반복 문제 풀이를 완료한다

--- src/ProblemString/StringRepetition.java
+++ src/ProblemString/StringRepetition.java
@@ -1,7 +1,28 @@
 package ProblemString;
 
-public class StringRepetition {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class StringRepetition {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int repetition = Integer.parseInt(stk.nextToken());
+            String repetitionWord = stk.nextToken();
+            char[] wordCharArr = repetitionWord.toCharArray();
+            for (char word : wordCharArr) {
+                for (int j = 0; j < repetition; j++) {
+                    bw.write(String.valueOf(word));
+                }
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
