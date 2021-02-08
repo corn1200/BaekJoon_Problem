64315495fec0485dfa11a4319d7873a8a0cd1e28
corn1200 <corn120033@gmail.com>
2021-02-09 08:47:34 +0900

    feat: 그룹 단어 체커 문제 풀이를 완료한다

--- src/ProblemString/GroupWordChecker.java
+++ src/ProblemString/GroupWordChecker.java
@@ -1,7 +1,37 @@
 package ProblemString;
 
-public class GroupWordChecker {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.HashSet;
 
+public class GroupWordChecker {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int groupWordsCount = 0;
+        rootLoop:
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            char[] wordCharArr = word.toCharArray();
+            char bufferAlp = '0';
+            HashSet<Character> alpSet = new HashSet<>();
+            for (char wordChar : wordCharArr) {
+                char tempAlp = bufferAlp;
+                bufferAlp = wordChar;
+                if (tempAlp != bufferAlp) {
+                    if (alpSet.contains(wordChar)) {
+                        continue rootLoop;
+                    }
+                    alpSet.add(wordChar);
+                }
+            }
+            groupWordsCount++;
+        }
+        br.close();
+
+        bw.write(String.valueOf(groupWordsCount));
+        bw.flush();
+        bw.close();
     }
 }
