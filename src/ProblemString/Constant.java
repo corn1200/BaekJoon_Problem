b18761e5aae4f5fdcc08e65471326dd68d9f8eaa
corn1200 <corn120033@gmail.com>
2021-02-09 01:43:02 +0900

    feat: 상수 문제 풀이를 완료한다

--- src/ProblemString/Constant.java
+++ src/ProblemString/Constant.java
@@ -1,7 +1,32 @@
 package ProblemString;
 
-public class Constant {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Constant {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        br.close();
+
+        StringBuffer number1 = new StringBuffer();
+        StringBuffer number2 = new StringBuffer();
+        number1.append(stk.nextToken());
+        number2.append(stk.nextToken());
+
+        int reverseNum1 = Integer.parseInt(
+                String.valueOf(number1.reverse()));
+        int reverseNum2 = Integer.parseInt(
+                String.valueOf(number2.reverse()));
+
+        if (reverseNum1 > reverseNum2) {
+            bw.write(String.valueOf(reverseNum1));
+        } else {
+            bw.write(String.valueOf(reverseNum2));
+        }
+        bw.flush();
+        bw.close();
     }
 }
