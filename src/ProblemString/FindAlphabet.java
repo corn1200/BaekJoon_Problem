a88df37a60e0cf21ab46f52c3e125e8c0ceab18d
corn1200 <corn120033@gmail.com>
2021-02-08 21:37:26 +0900

    feat: 알파벳 찾기 문제 풀이를 완료한다

--- src/ProblemString/FindAlphabet.java
+++ src/ProblemString/FindAlphabet.java
@@ -4,6 +4,20 @@ import java.io.*;
 
 public class FindAlphabet {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String S = br.readLine();
+        char[] alphabetArr = {'a', 'b', 'c', 'd',
+                'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        for (char alphabet : alphabetArr) {
+            bw.write(S.indexOf(alphabet) + " ");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
