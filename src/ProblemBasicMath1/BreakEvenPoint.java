0a95229e2fc961ff2c13421b13a02a5e6e6d56c9
corn1200 <corn120033@gmail.com>
2021-02-09 09:55:51 +0900

    feat: 손익분기점 문제 풀이를 완료한다

--- src/ProblemBasicMath1/BreakEvenPoint.java
+++ src/ProblemBasicMath1/BreakEvenPoint.java
@@ -1,7 +1,26 @@
 package ProblemBasicMath1;
 
-public class BreakEvenPoint {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class BreakEvenPoint {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        br.close();
+        int A = Integer.parseInt(stk.nextToken());
+        int B = Integer.parseInt(stk.nextToken());
+        int C = Integer.parseInt(stk.nextToken());
+
+        if (C > B) {
+            bw.write(String.valueOf(A / (C - B) + 1));
+        } else {
+            bw.write(String.valueOf(-1));
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
