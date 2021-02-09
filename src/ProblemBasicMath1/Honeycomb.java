8ef029de8e053af01e219f1ca615706f7ee86b46
corn1200 <corn120033@gmail.com>
2021-02-09 10:09:49 +0900

    feat: 벌집 문제 풀이를 완료한다

--- src/ProblemBasicMath1/Honeycomb.java
+++ src/ProblemBasicMath1/Honeycomb.java
@@ -1,7 +1,32 @@
 package ProblemBasicMath1;
 
-public class Honeycomb {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Honeycomb {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        bw.write(String.valueOf(passHexagonRoom(N)));
+        bw.flush();
+        bw.close();
+    }
+
+    public static int passHexagonRoom(int N) {
+        int over = 1;
+        if (N == 1) {
+            return 1;
+        }
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            int stack = i * 6;
+            if (stack + over >= N) {
+                return i + 1;
+            }
+            over += stack;
+        }
+        return 0;
     }
 }
