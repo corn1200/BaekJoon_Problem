8e7b5e836866639e809bd6799372b9f5bd519b90
corn1200 <corn120033@gmail.com>
2021-02-11 01:23:47 +0900

    feat: 달팽이는 올라가고 싶다 문제 풀이를 완료한다

--- src/ProblemBasicMath1/SnailWantGoUp.java
+++ src/ProblemBasicMath1/SnailWantGoUp.java
@@ -5,6 +5,19 @@ import java.util.StringTokenizer;
 
 public class SnailWantGoUp {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        double A = Integer.parseInt(stk.nextToken());
+        double B = Integer.parseInt(stk.nextToken());
+        double V = Integer.parseInt(stk.nextToken()) - A;
+        br.close();
+        double risePerDay = A - B;
+        int riseDayMax = (int) Math.ceil(V / risePerDay);
+
+        bw.write(String.valueOf(riseDayMax + 1));
+        bw.flush();
+        bw.close();
     }
 }
