fedbf55be071f7bcd5a07f91b41355d8b69e2c3e
corn1200 <corn120033@gmail.com>
2021-02-11 20:51:28 +0900

    feat: ACM 호텔 문제 풀이를 완료한다

--- src/ProblemBasicMath1/AcmHotel.java
+++ src/ProblemBasicMath1/AcmHotel.java
@@ -1,7 +1,29 @@
 package ProblemBasicMath1;
 
-public class AcmHotel {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class AcmHotel {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(stk.nextToken());
+            int W = Integer.parseInt(stk.nextToken());
+            int N = Integer.parseInt(stk.nextToken());
+            int floorNum = N % H;
+            if (floorNum == 0) {
+                floorNum = H;
+            }
+            int roomNum = (int) Math.ceil((double) N / H);
+            String viewZero = roomNum >= 10 ? "" : "0";
+            bw.write(floorNum + viewZero + roomNum + "\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
