c91818e5e52ba79ba672c23bed3dfb2b8615b1ab
corn1200 <corn120033@gmail.com>
2021-02-11 21:17:41 +0900

    feat: 부녀회장이 될테야 문제 규칙을 탐색한다

--- src/ProblemBasicMath1/BeWomenChairman.java
+++ src/ProblemBasicMath1/BeWomenChairman.java
@@ -1,7 +1,28 @@
 package ProblemBasicMath1;
 
-public class BeWomenChairman {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class BeWomenChairman {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int K = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+        }
+//        0층의 5호의 사람 5
+//        1 2 3 4 5
+//        1층의 5호의 사람 15 이전호 + 10
+//        1 3 6 10 15
+//        2층의 5호의 사람 35 이전호 + 20
+//        1 4 10 20 35
+//        3층의 5호의 사람 70 이전호 + 35
+//        1 5 15 35 70
+//        4층의 5호의 사람 126 이전호 + 56
+//        1 6 21 56 126
     }
 }
