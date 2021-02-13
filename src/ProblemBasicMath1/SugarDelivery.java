156670edc170685f864bd5eced417677564807d1
corn1200 <corn120033@gmail.com>
2021-02-13 21:01:30 +0900

    feat: 설탕 배달 문제 풀이를 완료한다

--- src/ProblemBasicMath1/SugarDelivery.java
+++ src/ProblemBasicMath1/SugarDelivery.java
@@ -1,7 +1,31 @@
 package ProblemBasicMath1;
 
-public class SugarDelivery {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class SugarDelivery {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        if (N % 5 == 0) {
+            bw.write(String.valueOf(N / 5));
+        } else {
+            int fiveKiloBagCount = (int) Math.floor((double) N / 5);
+            for (int i = fiveKiloBagCount; i >= 0; i--) {
+                int restWeight = N - (5 * i);
+                if (restWeight % 3 == 0) {
+                    bw.write(String.valueOf(i + (restWeight / 3)));
+                    break;
+                }
+                if (i == 0) {
+                    bw.write(String.valueOf(-1));
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
     }
 }
