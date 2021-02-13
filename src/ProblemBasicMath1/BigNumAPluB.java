4d148505c00a7fa12cdedd95c3e84e83b379dc53
corn1200 <corn120033@gmail.com>
2021-02-13 21:11:10 +0900

    feat: 큰 수 A+B 문제 풀이를 완료한다

--- src/ProblemBasicMath1/BigNumAPluB.java
+++ src/ProblemBasicMath1/BigNumAPluB.java
@@ -1,7 +1,21 @@
 package ProblemBasicMath1;
 
-public class BigNumAPluB {
-    public static void main(String[] args) {
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
+public class BigNumAPluB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        br.close();
+        BigInteger A = new BigInteger(stk.nextToken());
+        BigInteger B = new BigInteger(stk.nextToken());
+
+        bw.write(String.valueOf(A.add(B)));
+        bw.flush();
+        bw.close();
     }
 }
