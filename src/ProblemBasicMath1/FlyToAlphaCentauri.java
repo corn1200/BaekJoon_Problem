2cf5aecc84bd125f15bc19a5668d271152a2ac20
corn1200 <corn120033@gmail.com>
2021-02-14 19:51:51 +0900

    feat: Fly me to the Alpha Centauri 문제 규칙을 탐색한다

--- src/ProblemBasicMath1/FlyToAlphaCentauri.java
+++ src/ProblemBasicMath1/FlyToAlphaCentauri.java
@@ -1,7 +1,66 @@
 package ProblemBasicMath1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class FlyToAlphaCentauri {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+//        0 1
+//        1
+//        0 2
+//        1 1
+//        0 3
+//        1 1 1
+//        0 4
+//        1 2 1
+//        0 5
+//        1 1 2 1
+//        0 6
+//        1 2 2 1
+//        0 7
+//        1 1 2 2 1
+//        0 8
+//        1 2 2 2 1
+//        0 9
+//        1 2 3 2 1
+//        0 10
+//        1 1 2 3 2 1
+//        0 11
+//        1 2 2 3 2 1
+//        0 12
+//        1 2 3 3 2 1
+//        0 13
+//        1 1 2 3 3 2 1
+//        0 14
+//        1 2 2 3 3 2 1
+//        0 15
+//        1 2 3 3 3 2 1
+//        0 16
+//        1 2 3 4 3 2 1
+//        0 17
+//        1 1 2 3 4 3 2 1
+//        0 18
+//        1 2 2 3 4 3 2 1
+//        0 19
+//        1 2 3 3 4 3 2 1
+//        0 20
+//        1 2 3 4 4 3 2 1
+//        0 21
+//        1 1 2 3 4 4 3 2 1
+//        0 22
+//        1 2 2 3 4 4 3 2 1
+//        0 23
+//        1 2 3 3 4 4 3 2 1
+//        0 24
+//        1 2 3 4 4 4 3 2 1
+//        0 25
+//        1 2 3 4 5 4 3 2 1
+//        0 26
+//        1 1 2 3 4 5 4 3 2 1
     }
 }
