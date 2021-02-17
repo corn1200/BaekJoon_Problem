3e0e54d16b94b6a0a422d97dce2e1030aec4f2ac
corn1200 <corn120033@gmail.com>
2021-02-17 18:49:10 +0900

    feat: 소인수분해 문제 풀이를 완료한다

--- src/ProblemBasicMath1/Factorization.java
+++ src/ProblemBasicMath1/Factorization.java
@@ -1,7 +1,22 @@
 package ProblemBasicMath1;
 
-public class Factorization {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Factorization {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        for (int i = 2; i <= N && N != 1; i++) {
+            if (N % i == 0) {
+                N = N / i;
+                bw.write(i + "\n");
+                i = 1;
+            }
+        }
+        bw.flush();
+        bw.close();
     }
 }
