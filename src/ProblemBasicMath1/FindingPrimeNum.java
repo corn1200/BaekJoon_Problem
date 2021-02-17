5e499b591e9db73352356d235ef6aec556c32459
corn1200 <corn120033@gmail.com>
2021-02-17 19:05:39 +0900

    add: 백준 단계별 풀기 기본 수학 2 부문, 소수 구하기, 문제번호 1929번 파일을 추가한다

---
+++ src/ProblemBasicMath1/FindingPrimeNum.java
@@ -0,0 +1,11 @@
+package ProblemBasicMath1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class FindingPrimeNum {
+    public static void main(String[] args) {
+    }
+}
