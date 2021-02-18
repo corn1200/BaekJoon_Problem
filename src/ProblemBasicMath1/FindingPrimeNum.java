76d84bf9b3075dab53c8f63dedfbfa03a7b9d7e3
corn1200 <corn120033@gmail.com>
2021-02-18 14:47:45 +0900

    feat: 소수 구하기 문제 풀이 시간 초과가 발생한다

--- src/ProblemBasicMath1/FindingPrimeNum.java
+++ src/ProblemBasicMath1/FindingPrimeNum.java
@@ -1,11 +1,36 @@
 package ProblemBasicMath1;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class FindingPrimeNum {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(stk.nextToken());
+        int N = Integer.parseInt(stk.nextToken());
+        br.close();
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 2; i <= N; i++) {
+            list.addLast(i);
+        }
+
+        while (list.size() > 0) {
+            int num = list.removeFirst();
+            if (num >= M) {
+                bw.write(num + "\n");
+            }
+            for (int i = 2; i * num <= N; i++) {
+                if (list.contains(i * num)) {
+                    list.remove(list.indexOf(i * num));
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
     }
 }
