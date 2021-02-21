c81127cbb0dc02bb1a29425f4da857c73fafab80
corn1200 <corn120033@gmail.com>
2021-02-21 21:40:03 +0900

    feat: 골드바흐의 추측 문제 풀이를 완료한다

--- src/ProblemBasicMath2/GoldbachGuess.java
+++ src/ProblemBasicMath2/GoldbachGuess.java
@@ -1,7 +1,47 @@
 package ProblemBasicMath2;
 
-public class GoldbachGuess {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Arrays;
 
+public class GoldbachGuess {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int maxNum = 0;
+        int T = Integer.parseInt(br.readLine());
+        int[] numArr = new int[T];
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            numArr[i] = n;
+            if (maxNum < n) {
+                maxNum = n;
+            }
+        }
+        br.close();
+
+        boolean[] primeArr = new boolean[maxNum - 1];
+        Arrays.fill(primeArr, true);
+        primeArr[0] = false;
+        primeArr[1] = false;
+
+        for (long i = 2; i < primeArr.length; i++) {
+            if (primeArr[(int) i]) {
+                for (long j = i * i; j < primeArr.length; j += i) {
+                    primeArr[(int) j] = false;
+                }
+            }
+        }
+
+        for (int i = 0; i < numArr.length; i++) {
+            for (int j = numArr[i] / 2; j >= 2; j--) {
+                if (primeArr[j] && primeArr[numArr[i] - j]) {
+                    bw.write(j + " " + (numArr[i] - j) + "\n");
+                    break;
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
     }
 }
