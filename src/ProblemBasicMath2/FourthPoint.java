5eb914be225c52b3f71ceeba251b85ee7f777f93
corn1200 <corn120033@gmail.com>
2021-02-22 17:25:47 +0900

    feat: 네 번째 점 문제 풀이를 완료한다

--- src/ProblemBasicMath2/FourthPoint.java
+++ src/ProblemBasicMath2/FourthPoint.java
@@ -1,7 +1,26 @@
 package ProblemBasicMath2;
 
-public class FourthPoint {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class FourthPoint {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] locationX = new int[3];
+        int[] locationY = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            locationX[i] = Integer.parseInt(stk.nextToken());
+            locationY[i] = Integer.parseInt(stk.nextToken());
+        }
+        int pointX = locationX[0] == locationX[1] ? locationX[2] : locationX[0] == locationX[2] ? locationX[1] : locationX[0];
+        int pointY = locationY[0] == locationY[1] ? locationY[2] : locationY[0] == locationY[2] ? locationY[1] : locationY[0];
+
+        bw.write(pointX + " " + pointY);
+        bw.flush();
+        bw.close();
     }
 }
