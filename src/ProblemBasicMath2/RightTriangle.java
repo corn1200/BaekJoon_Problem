4073c76250e7f441d189306da2c6c89bc54903de
corn1200 <corn120033@gmail.com>
2021-02-23 15:10:35 +0900

    feat: 직각삼각형 문제 풀이를 완료한다

--- src/ProblemBasicMath2/RightTriangle.java
+++ src/ProblemBasicMath2/RightTriangle.java
@@ -5,6 +5,30 @@ import java.util.StringTokenizer;
 
 public class RightTriangle {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String input;
+        while ((input = br.readLine()) != null) {
+            StringTokenizer stk = new StringTokenizer(input);
+            int sideOne = Integer.parseInt(stk.nextToken());
+            int sideTwo = Integer.parseInt(stk.nextToken());
+            int sideThree = Integer.parseInt(stk.nextToken());
+            if (sideOne == 0 && sideTwo == 0 && sideThree == 0) {
+                break;
+            } else if (sideOne > sideTwo && sideOne > sideThree) {
+                bw.write((sideOne * sideOne == (sideTwo * sideTwo) +
+                        (sideThree * sideThree) ? "right" : "wrong") + "\n");
+            } else if (sideTwo > sideOne && sideTwo > sideThree) {
+                bw.write((sideTwo * sideTwo == (sideOne * sideOne) +
+                        (sideThree * sideThree) ? "right" : "wrong") + "\n");
+            } else {
+                bw.write((sideThree * sideThree == (sideTwo * sideTwo) +
+                        (sideOne * sideOne) ? "right" : "wrong") + "\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
