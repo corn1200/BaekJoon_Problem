1d54729a671445999c52c47deef6daea8f765e00
corn1200 <corn120033@gmail.com>
2021-02-23 15:10:07 +0900

    add: 백준 단계별 풀기 기본 수학 2 부문, 직각삼각형, 문제번호 4153번 파일을 추가한다

---
+++ src/ProblemBasicMath2/RightTriangle.java
@@ -0,0 +1,10 @@
+package ProblemBasicMath2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class RightTriangle {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
