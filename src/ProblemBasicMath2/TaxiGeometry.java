25c48ccf56a36f13115d93199a6163a2ed3cc0e8
corn1200 <corn120033@gmail.com>
2021-02-25 21:36:25 +0900

    feat: 택시 기하학 문제 풀이를 완료한다

--- src/ProblemBasicMath2/TaxiGeometry.java
+++ src/ProblemBasicMath2/TaxiGeometry.java
@@ -1,7 +1,18 @@
 package ProblemBasicMath2;
 
-public class TaxiGeometry {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class TaxiGeometry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        double R = Double.parseDouble(br.readLine());
+        br.close();
+
+        bw.write(String.format("%.6f", R * R * Math.PI) + "\n");
+        bw.write(String.format("%.6f", R * R * 2.0));
+        bw.flush();
+        bw.close();
     }
 }
