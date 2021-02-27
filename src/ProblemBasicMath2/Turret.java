afdea8052d36b16746f3ad8e1c16917391ddf11e
corn1200 <corn120033@gmail.com>
2021-02-27 12:48:47 +0900

    bug: 1002번 터렛 문제 테스트 케이스 확인 불가 오류 발생

--- src/ProblemBasicMath2/Turret.java
+++ src/ProblemBasicMath2/Turret.java
@@ -1,7 +1,41 @@
 package ProblemBasicMath2;
 
-public class Turret {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Turret {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(stk.nextToken());
+            int y1 = Integer.parseInt(stk.nextToken());
+            int r1 = Integer.parseInt(stk.nextToken());
+            int x2 = Integer.parseInt(stk.nextToken());
+            int y2 = Integer.parseInt(stk.nextToken());
+            int r2 = Integer.parseInt(stk.nextToken());
+            if (x1 == x2 && y1 == y2) {
+                if (r1 == r2) {
+                    bw.write(-1 + "\n");
+                } else {
+                    bw.write(0 + "\n");
+                }
+            } else if ((x1 - x2) * (x1 - x2) == (r1 + r2) * (r1 + r2) &&
+                    ((y1 == 0 && y2 == 0) || (y1 - y2) * (y1 - y2) >=
+                    (r1 + r2) * (r1 + r2)) || (y1 - y2) * (y1 - y2) ==
+                    (r1 + r2) * (r1 + r2) && ((x1 == 0 && x2 == 0) ||
+                    (x1 - x2) * (x1 - x2) >= (r1 + r2) * (r1 + r2))) {
+                bw.write(1 + "\n");
+            } else if ((x1 - x2) * (x1 - x2) <= (r1 + r2) * (r1 + r2) &&
+                    (y1 - y2) * (y1 - y2) <= (r1 + r2) * (r1 + r2)) {
+                bw.write(2 + "\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
