09b4398d053b0a0059ad898cae2c48ab53d59f23
corn1200 <corn120033@gmail.com>
2021-02-28 17:55:00 +0900

    feat: 1002번 터렛 문제 풀이를 완료한다

--- src/ProblemBasicMath2/Turret.java
+++ src/ProblemBasicMath2/Turret.java
@@ -17,21 +17,18 @@ public class Turret {
             int x2 = Integer.parseInt(stk.nextToken());
             int y2 = Integer.parseInt(stk.nextToken());
             int r2 = Integer.parseInt(stk.nextToken());
+//            두 점 사이의 거리
+            double d = Math.sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
             if (x1 == x2 && y1 == y2) {
-                if (r1 == r2) {
-                    bw.write(-1 + "\n");
-                } else {
-                    bw.write(0 + "\n");
-                }
-            } else if ((x1 - x2) * (x1 - x2) == (r1 + r2) * (r1 + r2) &&
-                    ((y1 == 0 && y2 == 0) || (y1 - y2) * (y1 - y2) >=
-                    (r1 + r2) * (r1 + r2)) || (y1 - y2) * (y1 - y2) ==
-                    (r1 + r2) * (r1 + r2) && ((x1 == 0 && x2 == 0) ||
-                    (x1 - x2) * (x1 - x2) >= (r1 + r2) * (r1 + r2))) {
+                bw.write((r1 == r2 ? -1 : 0) + "\n");
+            } else if (r1 + r2 < d || d < Math.abs(r1 - r2)) {
+                bw.write(0 + "\n");
+            } else if (d == r1 + r2 || d == Math.abs(r1 - r2)) {
                 bw.write(1 + "\n");
-            } else if ((x1 - x2) * (x1 - x2) <= (r1 + r2) * (r1 + r2) &&
-                    (y1 - y2) * (y1 - y2) <= (r1 + r2) * (r1 + r2)) {
+            } else if (Math.abs(r1 - r2) < d && d < r1 + r2) {
                 bw.write(2 + "\n");
+            } else {
+                bw.write(-1 + "\n");
             }
         }
         br.close();
