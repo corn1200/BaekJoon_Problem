7c37a5d48290bf4d88176b26fb1dc253149a08c9
corn1200 <corn120033@gmail.com>
2021-02-28 18:07:13 +0900

    feat: 10872번 팩토리얼 문제 풀이를 완료한다

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -1,7 +1,24 @@
 package Recursion;
 
-public class Factorial {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Factorial {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        bw.write(String.valueOf(factorial(1, N)));
+        bw.flush();
+        bw.close();
+    }
+
+    static int factorial(int n, int stack) {
+        if (stack <= 0) {
+            return n;
+        }
+        n *= stack;
+        stack--;
+        return factorial(n, stack);
     }
 }
