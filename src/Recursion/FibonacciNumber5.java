c25f1d4c2a081003cd9df5da505af4ea7762c1ee
corn1200 <corn120033@gmail.com>
2021-03-01 18:01:51 +0900

    feat: 10870번 피보나치 수 5 문제 풀이를 완료한다

--- src/Recursion/FibonacciNumber5.java
+++ src/Recursion/FibonacciNumber5.java
@@ -1,7 +1,24 @@
 package Recursion;
 
+import java.io.*;
+
 public class FibonacciNumber5 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        bw.write(String.valueOf(fibonacciNum(0, 1, n)));
+        bw.flush();
+        bw.close();
+    }
+
+    static int fibonacciNum(int n1, int n2, int stack) {
+        if (stack == 0) {
+            return n1;
+        }
+        stack--;
+        return fibonacciNum(n2, n1 + n2, stack);
     }
 }
