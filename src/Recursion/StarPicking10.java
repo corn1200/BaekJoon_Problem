3f66da4eab4fa664dea1296ae61444b32864668b
corn1200 <corn120033@gmail.com>
2021-03-02 19:01:23 +0900

    feat: 2447번 별 찍기 - 10 문제 풀이를 완료한다

--- src/Recursion/StarPicking10.java
+++ src/Recursion/StarPicking10.java
@@ -1,7 +1,53 @@
 package Recursion;
 
-public class StarPicking10 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Arrays;
 
+public class StarPicking10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        char[][] starArr = new char[N][N];
+        for (int i = 0; i < starArr.length; i++) {
+            Arrays.fill(starArr[i], ' ');
+        }
+
+        recursiveStarPicking(N, 0, 0, starArr);
+
+        for (char[] arr : starArr) {
+            for (char star : arr) {
+                bw.write(String.valueOf(star));
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static void recursiveStarPicking(int n, int firstIndex, int secondIndex, char[][] starArr) {
+        int stack = n / 3;
+        starArr[firstIndex][secondIndex] = '*';
+        starArr[firstIndex][secondIndex + stack] = '*';
+        starArr[firstIndex][secondIndex + (stack * 2)] = '*';
+        starArr[firstIndex + stack][secondIndex] = '*';
+        starArr[firstIndex + stack][secondIndex + (stack * 2)] = '*';
+        starArr[firstIndex + (stack * 2)][secondIndex] = '*';
+        starArr[firstIndex + (stack * 2)][secondIndex + stack] = '*';
+        starArr[firstIndex + (stack * 2)][secondIndex + (stack * 2)] = '*';
+        if (n == 3) {
+            return;
+        } else {
+            recursiveStarPicking(stack, firstIndex, secondIndex, starArr);
+            recursiveStarPicking(stack, firstIndex, secondIndex + stack, starArr);
+            recursiveStarPicking(stack, firstIndex, secondIndex + (stack * 2), starArr);
+            recursiveStarPicking(stack, firstIndex + stack, secondIndex, starArr);
+            recursiveStarPicking(stack, firstIndex + stack, secondIndex + (stack * 2), starArr);
+            recursiveStarPicking(stack, firstIndex + (stack * 2), secondIndex, starArr);
+            recursiveStarPicking(stack, firstIndex + (stack * 2), secondIndex + stack, starArr);
+            recursiveStarPicking(stack, firstIndex + (stack * 2), secondIndex + (stack * 2), starArr);
+        }
     }
 }
