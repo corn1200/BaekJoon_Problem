c60f642c49ef8c1405822dad90e1f147cf6ad0ea
corn1200 <corn120033@gmail.com>
2021-03-03 21:34:16 +0900

    bug: 2798번 블랙잭 문제 테스트 케이스 확인 불가 오류 발생

--- src/BruteForce/Blackjack.java
+++ src/BruteForce/Blackjack.java
@@ -1,7 +1,41 @@
 package BruteForce;
 
-public class Blackjack {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Blackjack {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int max = 0;
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(stk.nextToken());
+        int M = Integer.parseInt(stk.nextToken());
+        StringTokenizer cardStk = new StringTokenizer(br.readLine());
+        br.close();
+        int[] cardArr = new int[N];
+        for (int i = 0; i < cardArr.length; i++) {
+            cardArr[i] = Integer.parseInt(cardStk.nextToken());
+        }
+
+        for (int i = 0; i < cardArr.length; i++) {
+            int num = cardArr[i];
+            for (int j = i + 1; j < cardArr.length; j++) {
+                if (num + cardArr[j] >= M) {
+                    continue;
+                }
+                num = cardArr[i] + cardArr[j];
+                for (int k = j + 1; k < cardArr.length; k++) {
+                    if (num + cardArr[k] <= M && num + cardArr[k] > max) {
+                        max = num + cardArr[k];
+                    }
+                }
+            }
+        }
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
     }
 }
