4b6b5e13cb762ae08fbb8b51cd2030c52d9d4177
corn1200 <cornn120033@gmail.com>
2021-03-04 16:20:08 +0900

    feat: 11729번 하노이 탑 이동 순서 문제 풀이를 완료한다

--- src/Recursion/HanoiTowerTransferOrder.java
+++ src/Recursion/HanoiTowerTransferOrder.java
@@ -1,7 +1,29 @@
 package Recursion;
 
-public class HanoiTowerTransferOrder {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class HanoiTowerTransferOrder {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        bw.write((int) (Math.pow(2, N) - 1) + "\n");
+        hanoiTowerTransferOrder(1, 3, N);
+        bw.flush();
+        bw.close();
+    }
+
+    static int hanoiTowerTransferOrder(int from, int to, int n) throws IOException {
+        if (n == 1) {
+            bw.write(from + " " + to + "\n");
+            return 0;
+        }
+        hanoiTowerTransferOrder(from, 6 - from - to, n - 1);
+        bw.write(from + " " + to + "\n");
+        hanoiTowerTransferOrder(6 - from - to, to, n - 1);
+        return 0;
     }
 }
