0aed88915f9b552d13a6653707223b6c2ee4d6aa
corn1200 <corn120033@gmail.com>
2021-03-09 16:53:59 +0900

    feat: 1018번 체스판 다시 칠하기 문제 풀이를 완료한다

--- src/BruteForce/RepaintingChessboard.java
+++ src/BruteForce/RepaintingChessboard.java
@@ -11,26 +11,55 @@ public class RepaintingChessboard {
         StringTokenizer stk = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(stk.nextToken());
         int M = Integer.parseInt(stk.nextToken());
-        int blackCount = 0;
-        int whiteCount = 0;
+        int minWrongChess = Integer.MAX_VALUE;
+        char[][] chessArr = new char[N][M];
+        char[][] rightChessBoard1 = {
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}};
+        char[][] rightChessBoard2 = {
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}};
 
-        for (int i = 0; i < N; i++) {
-            char[] chessArr = br.readLine().toCharArray();
-            for (int j = 0; j < chessArr.length; j++) {
-                if (chessArr[j] == 'B') {
-                    blackCount++;
-                } else {
-                    whiteCount++;
+        for (int i = 0; i < chessArr.length; i++) {
+            chessArr[i] = br.readLine().toCharArray();
+        }
+        br.close();
+
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                int wrongChess1 = 0;
+                int wrongChess2 = 0;
+                for (int k = i; k < i + 8; k++) {
+                    for (int l = j; l < j + 8; l++) {
+                        if (chessArr[k][l] != rightChessBoard1[k - i][l - j]) {
+                            wrongChess1++;
+                        }
+                        if (chessArr[k][l] != rightChessBoard2[k - i][l - j]) {
+                            wrongChess2++;
+                        }
+                    }
+                }
+                if (wrongChess1 < minWrongChess) {
+                    minWrongChess = wrongChess1;
+                }
+                if (wrongChess2 < minWrongChess) {
+                    minWrongChess = wrongChess2;
                 }
             }
         }
-
-        if (Math.abs(N * M / 2 - blackCount) < Math.abs(N * M / 2 - whiteCount)) {
-            bw.write(String.valueOf(Math.abs(N * M / 2 - blackCount)));
-        } else {
-            bw.write(String.valueOf(Math.abs(N * M / 2 - whiteCount)));
-        }
-
+        bw.write(String.valueOf(minWrongChess));
         bw.flush();
         bw.close();
     }
