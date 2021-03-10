9608601083bcb14bbda81ac2cc3e22d0db67d31b
corn1200 <corn120033@gmail.com>
2021-03-10 18:14:21 +0900

    bug: 1436번 영화감독 숌 문제 테스트 케이스 확인 불가 오류 발생

--- src/BruteForce/FilmDirectorShum.java
+++ src/BruteForce/FilmDirectorShum.java
@@ -1,7 +1,16 @@
 package BruteForce;
 
-public class FilmDirectorShum {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class FilmDirectorShum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        bw.write((N - 1 == 0 ? "" : N - 1) + "666");
+        bw.flush();
+        bw.close();
     }
 }
