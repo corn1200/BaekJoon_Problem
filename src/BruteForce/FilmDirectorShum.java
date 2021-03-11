f5797bc3ee5f6c0c0277d64173bbfeb9c4f9990e
corn1200 <corn120033@gmail.com>
2021-03-11 20:38:14 +0900

    feat: 1436번 영화감독 숌 문제 풀이를 완료한다

--- src/BruteForce/FilmDirectorShum.java
+++ src/BruteForce/FilmDirectorShum.java
@@ -8,59 +8,20 @@ public class FilmDirectorShum {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int count = 1;
-        String result = "666";
+        int count = 0;
+        int result = 0;
 
-        if (N != 1) {
-            for (int i = 1; i < Integer.MAX_VALUE; i++) {
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            if (String.valueOf(i).contains("666")) {
                 count++;
-                char[] numDigit = String.valueOf(i).toCharArray();
-                if (numDigit[numDigit.length - 1] != '6') {
-                    result = i + "666";
-                } else {
-                    int sixCount = 0;
-                    for (int j = numDigit.length - 1; j >= 0; j--) {
-                        if (numDigit[j] == '6') {
-                            sixCount++;
-                            continue;
-                        }
-                        break;
-                    }
-                    for (int j = 0; j < Math.pow(10, sixCount); j++) {
-                        result = String.valueOf((int) (666 * Math.pow(10, sixCount)) + j);
-                        for (int k = 0; k < numDigit.length - sixCount; k++) {
-                            result = numDigit[k] + result;
-                        }
-                        if (count == N || j == Math.pow(10, sixCount) - 1) {
-                            break;
-                        }
-                        count++;
-                    }
-                }
-                if (count == N) {
-                    break;
-                }
+            }
+            if (count == N) {
+                result = i;
+                break;
             }
         }
-        bw.write(result);
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
-//        666
-//        1666
-//        2666
-//        3666
-//        4666
-//        5666
-//        6660
-//        6661
-//        6662
-//        6663
-//        6664
-//        6665
-//        6666
-//        6667
-//        6668
-//        6669
-//        7666
     }
 }
