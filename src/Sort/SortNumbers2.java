5da02e40e0f557a5d2cf575c90023603124397ab
corn1200 <corn120033@gmail.com>
2021-03-12 08:36:27 +0900

    feat: 2751번 수 정렬하기2 문제 풀이를 완료한다

--- src/Sort/SortNumbers2.java
+++ src/Sort/SortNumbers2.java
@@ -5,6 +5,19 @@ import java.util.PriorityQueue;
 
 public class SortNumbers2 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            queue.add(Integer.valueOf(br.readLine()));
+        }
+        for (int i = 0; i < N; i++) {
+            bw.write(queue.poll() + "\n");
+        }
+        bw.flush();
+        bw.close();
+//        똑같은 문제
     }
 }
