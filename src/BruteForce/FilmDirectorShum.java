2bc2fcffa2c65a0f6c6854af50a7728165af2e3c
corn1200 <corn120033@gmail.com>
2021-03-11 17:56:46 +0900

    bug: 1436번 영화감독 숌 문제 테스트 케이스 확인 불가 오류 발생/규칙 재확인

--- src/BruteForce/FilmDirectorShum.java
+++ src/BruteForce/FilmDirectorShum.java
@@ -8,9 +8,59 @@ public class FilmDirectorShum {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        String result = "666";
 
-        bw.write((N - 1 == 0 ? "" : N - 1) + "666");
+        if (N != 1) {
+            for (int i = 1; i < Integer.MAX_VALUE; i++) {
+                count++;
+                char[] numDigit = String.valueOf(i).toCharArray();
+                if (numDigit[numDigit.length - 1] != '6') {
+                    result = i + "666";
+                } else {
+                    int sixCount = 0;
+                    for (int j = numDigit.length - 1; j >= 0; j--) {
+                        if (numDigit[j] == '6') {
+                            sixCount++;
+                            continue;
+                        }
+                        break;
+                    }
+                    for (int j = 0; j < Math.pow(10, sixCount); j++) {
+                        result = String.valueOf((int) (666 * Math.pow(10, sixCount)) + j);
+                        for (int k = 0; k < numDigit.length - sixCount; k++) {
+                            result = numDigit[k] + result;
+                        }
+                        if (count == N || j == Math.pow(10, sixCount) - 1) {
+                            break;
+                        }
+                        count++;
+                    }
+                }
+                if (count == N) {
+                    break;
+                }
+            }
+        }
+        bw.write(result);
         bw.flush();
         bw.close();
+//        666
+//        1666
+//        2666
+//        3666
+//        4666
+//        5666
+//        6660
+//        6661
+//        6662
+//        6663
+//        6664
+//        6665
+//        6666
+//        6667
+//        6668
+//        6669
+//        7666
     }
 }
