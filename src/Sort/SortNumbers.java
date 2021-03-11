3b3a4a3c8ca34161f00188b341ad11a36b8fc133
corn1200 <corn120033@gmail.com>
2021-03-11 21:08:02 +0900

    feat: 2750번 수 정렬하기 문제 풀이를 완료한다

--- src/Sort/SortNumbers.java
+++ src/Sort/SortNumbers.java
@@ -1,7 +1,22 @@
 package Sort;
 
-public class SortNumbers {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.PriorityQueue;
 
+public class SortNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            queue.add(Integer.valueOf(br.readLine()));
+        }
+        for (int i = 0; i < N; i++) {
+            bw.write(queue.poll() + "\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
