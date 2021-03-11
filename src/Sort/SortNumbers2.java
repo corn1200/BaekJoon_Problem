54e727dc49ac4248ed2de1029e3a42f17bb04c1f
corn1200 <corn120033@gmail.com>
2021-03-11 21:10:48 +0900

    add: 백준 단계별 풀기 정렬 부문, 수 정렬하기 2, 문제번호 2751번 파일을 추가한다

---
+++ src/Sort/SortNumbers2.java
@@ -0,0 +1,22 @@
+package Sort;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class SortNumbers2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            queue.add(Integer.valueOf(br.readLine()));
+        }
+        for (int i = 0; i < N; i++) {
+            bw.write(queue.poll() + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
