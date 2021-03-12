4d41f577eaf55a01a81bddfc840a52db8bcfa66e
corn1200 <corn120033@gmail.com>
2021-03-12 09:15:33 +0900

    feat: 10989번 수 정렬하기 3 문제 풀이를 완료한다

--- src/Sort/SortNumbers3.java
+++ src/Sort/SortNumbers3.java
@@ -8,15 +8,26 @@ public class SortNumbers3 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
+        int[] inputArr = new int[N];
         int[] countArr = new int[10001];
         int[] outputArr = new int[N];
 
         for (int i = 0; i < N; i++) {
-            countArr[Integer.parseInt(br.readLine())]++;
+            inputArr[i] = Integer.parseInt(br.readLine());
+            countArr[inputArr[i]]++;
         }
 
         for (int i = 1; i < countArr.length; i++) {
-            countArr[1] += countArr[i - 1];
+            countArr[i] += countArr[i - 1];
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            outputArr[countArr[inputArr[i]] - 1] = inputArr[i];
+            countArr[inputArr[i]]--;
+        }
+
+        for (int i = 0; i < outputArr.length; i++) {
+            bw.write(outputArr[i] + "\n");
         }
 
         bw.flush();
