a5f5c561dbce1cca4c3bc15d877f71492713323e
corn1200 <corn120033@gmail.com>
2021-03-14 02:31:42 +0900

    feat: 2108번 통계학 문제 풀이를 완료한다

--- src/Sort/Statistics.java
+++ src/Sort/Statistics.java
@@ -1,42 +1,59 @@
 package Sort;
 
 import java.io.*;
-import java.util.*;
+import java.util.TreeMap;
 
 public class Statistics {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int median = 0;
         double total = 0;
         int modeMinSecond = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            total += num;
-            if (num < min) {
-                min = num;
-            }
-            if (num > max) {
-                max = num;
-            }
-            queue.add(num);
+        int modeMax = 0;
+        int N = Integer.parseInt(br.readLine());
+        int[] inputArr = new int[N];
+        int[] countArr = new int[8001];
+        int[] countArrClone;
+        int[] outputArr = new int[N];
+        for (int i = 0; i < inputArr.length; i++) {
+            inputArr[i] = Integer.parseInt(br.readLine());
+            total += inputArr[i];
+            countArr[inputArr[i] + 4000]++;
         }
 
-        for (int i = 0; i < N; i++) {
-            int num = queue.poll();
-            if (i < N / 2 + 1) {
-                median = num;
+        countArrClone = countArr.clone();
+
+        for (int i = 1; i < countArr.length; i++) {
+            countArr[i] += countArr[i - 1];
+        }
+
+        for (int i = inputArr.length - 1; i >= 0; i--) {
+            outputArr[countArr[inputArr[i] + 4000] - 1] = inputArr[i];
+            countArr[inputArr[i] + 4000]--;
+        }
+
+        for (int i = 0; i < countArrClone.length; i++) {
+            if (countArrClone[i] > modeMax) {
+                modeMax = countArrClone[i];
+            }
+        }
+
+        int stack = 0;
+        for (int i = 0; i < outputArr.length; i++) {
+            if (stack >= 2) {
+                break;
+            } else if (countArrClone[outputArr[i] + 4000] == modeMax &&
+                    outputArr[i] != modeMinSecond) {
+                modeMinSecond = outputArr[i];
+                stack++;
             }
         }
 
         bw.write(String.format("%.0f", total / N) + "\n");
-        bw.write(median + "\n");
-        bw.write(max - min + "\n");
+        bw.write(outputArr[N / 2] + "\n");
+        bw.write(modeMinSecond + "\n");
+        bw.write(outputArr[outputArr.length - 1] - outputArr[0] + "\n");
         bw.flush();
         bw.close();
     }
