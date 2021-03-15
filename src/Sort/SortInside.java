022edc8eedce712b41ffa753088ea01be4f4628d
corn1200 <corn120033@gmail.com>
2021-03-15 13:28:31 +0900

    feat: 1427번 소트인사이드 문제 풀이를 완료한다

--- src/Sort/SortInside.java
+++ src/Sort/SortInside.java
@@ -1,7 +1,31 @@
 package Sort;
 
-public class SortInside {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class SortInside {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] N = br.readLine().toCharArray();
+        int[] countArr = new int[10];
+        char[] outputArr = new char[N.length];
+
+        for (int i = 0; i < N.length; i++) {
+            countArr[Integer.parseInt(String.valueOf(N[i]))]++;
+        }
+
+        for (int i = 1; i < countArr.length; i++) {
+            countArr[i] += countArr[i - 1];
+        }
+
+        for (int i = N.length - 1; i >= 0; i--) {
+            outputArr[countArr[Integer.parseInt(String.valueOf(N[i]))] - 1] = N[i];
+            countArr[Integer.parseInt(String.valueOf(N[i]))]--;
+        }
+
+        bw.write(new StringBuffer(new String(outputArr)).reverse().toString());
+        bw.flush();
+        bw.close();
     }
 }
