fb43169d6835c6816b0f02f0f671066c6f91a0a9
corn1200 <corn120033@gmail.com>
2021-03-16 17:15:39 +0900

    add: 백준 단계별 풀기 정렬 부문, 좌표 정렬하기2, 문제번호 11651번 파일을 추가한다

---
+++ src/Sort/AligningCoordinates2.java
@@ -0,0 +1,11 @@
+package Sort;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class AligningCoordinates2 {
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
