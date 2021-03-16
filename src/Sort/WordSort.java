f11e4e22996142a4897172ce1d6e7e96b0199b9a
corn1200 <corn120033@gmail.com>
2021-03-16 18:00:37 +0900

    save: at 2021-03-16

--- src/Sort/WordSort.java
+++ src/Sort/WordSort.java
@@ -1,7 +1,47 @@
 package Sort;
 
-public class WordSort {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class WordSort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        String[] strArr = new String[N];
+        for (int i = 0; i < strArr.length; i++) {
+            strArr[i] = br.readLine();
+        }
+        br.close();
+
+        quickSort(strArr, 0, strArr.length);
+
+        bw.flush();
+        bw.close();
+    }
+
+    static void quickSort(String[] strArr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int pi = partition(strArr, left, right);
+        quickSort(strArr, left, pi - 1);
+        quickSort(strArr, pi + 1, right);
+    }
+
+    static int partition(String[] strArr, int left, int right) {
+        String pivot = strArr[(left + right) / 2];
+        int i = left, j = right;
+
+        while (i < j) {
+            while (pivot.length() < strArr[j].length()) {
+                j--;
+            }
+
+            while (i < j && pivot.length() >= strArr[i].length()) {
+                i++;
+            }
+        }
     }
 }
